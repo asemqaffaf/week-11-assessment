0b927c92fd381c827159f15d4ab3caed7e3ee0c7
asemqaffaf <asem@qaffaf.com>
2019-09-01 10:55:42 +0300

    week-11

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 
 /* Question 1 : Write a PHP script to display names and the salaries amounts, within a table. ou should use `echo ` to generate your table. */
 //////////////////////// Your Code Here /////////////////////
-
+// echo "Name";
+echo "<table><tr> <td> Salary for mr.A : </td><td> $2000</td> </tr> </table>
+            <tr><td> Salary for mr.B : </td></tr> <td>$7000</td> </tr>
+" ;
 
 
 
@@ -17,6 +20,13 @@ $countries = array( "Italy"=>"Rome",
                "Finland"=>"Helsinki",
                "France" => "Paris",
                "Slovakia"=>"Bratislava" ) ;
+sort($countries);
+    // print_r($countries);
+for ($i=0; $i < sizeof($countries) ; $i++) { 
+// print_r ($countries);
+// echo array_keys($countries)[$i];
+echo "<ul>". $countries[array_keys($countries)[$i]] ." ". array_values($countries)[$i] ."</ul>";
+}
 
 /*Question 3 : Write a script to build the following stars pattern, using a nested for loop.
 
@@ -32,10 +42,30 @@ $countries = array( "Italy"=>"Rome",
 *
 
 */
+$counter = 0;
+for ($i=0; $i < 10; $i++) { 
+    echo "*<br>";
+    for ($j=0; $j <= i; $j++) { 
+        echo "*";
+     
+      
+    }
+}
 
 /*Question 4 : Write a PHP script to calculate the difference between two dates.
 Input : 1981-11-04, 2013-09-04
 Output : 31 years, 10 months, 11 days
 
 
+
+
 /*Question 5 : Create a simple HTML form that accept the user name after submitting display the name using PHP echo statement under the form. */
+
+
+
+$first_date = date(1981-11-04);
+// $second_date  = date(2013-09-04);
+
+echo "<form>username: <input></input> <br/> password: <input></input><br/><button>submit</button></form> "
+
+?>
